3ef02e7dbc33f2d5c5e5a6a4bdfb053a382fa9ad
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-06-22 00:08:23 +0200

    Creado login y home
    Creado login funcionando y redirecciones a home desde register y login si ya estas logeado

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,16 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     public function show()
     {
+        // If user is logged in, redirect to home page
+        if(Auth::check()) {
+            return redirect('/home');
+        }
         return view('auth.register');
     }
 
     public function register(RegisterRequest $request)
     {
         $user = User::create($request->validated());
+        return redirect('/login')->with('success', 'Account created successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +20,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', function () {
-    return view('auth.register');
-});
+Route::get('/register', [RegisterController::class, 'show']);
 
 Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/login', [LoginController::class, 'show']);
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/home', [HomeController::class, 'index']);
