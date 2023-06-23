617d5dac85c715279f787d3acc551d67a15ade16
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-06-23 15:59:37 +0200

    Logout
    Creado controlador del logout

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,6 +10,18 @@
 
 <body>
     <h1>Home</h1>
+
+    @auth
+        <p>Bienvenido {{ auth()->user()->name ?? auth()->user()->username }} estás autenticado a la página</p>
+        <p>
+            <a href="/logout">Logout</a>
+        </p>
+    @endauth
+
+    @guest
+        <p>Para ver el contenido <a href="/login">Inicia sesión</a></p>
+    @endguest
+
 </body>
 
 </html>
