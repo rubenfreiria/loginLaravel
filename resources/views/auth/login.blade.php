92a6f8d91f6e371993bcd705a935bd139802d4f3
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-06-24 00:29:31 +0200

    Formularios de registro y login acabados
    Formularios con estilos. Faltaria mostrar errores

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
+@extends('layouts.auth-master')
+
+@section('content')
     <form action="/login" method="POST">
         @csrf
-        username/email
-        <input type="text" name="username" id="">
-
-        password
-        <input type="password" name="password" id="">
-
-        <input type="submit" value="Login">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Username / Email address</label>
+            <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <a href="/register">Crete account</a>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-</body>
-</html>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.auth-master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
-</head>
-
-<body>
+@section('content')
     <form action="/register" method="POST">
         @csrf
-        <input type="text" name="username">
-        <input type="email" name="email">
-        <input type="password" name="password">
-        <input type="password" name="password_confirmation">
-        <button type="submit">Registrarse</button>
+        <div class="form-floating mb-3">
+            <input type="email" placeholder="Email" name="email" class="form-control" id="exampleInputEmail1"
+                aria-describedby="emailHelp">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="text" placeholder="User name" name="username" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Username</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" placeholder="Password" name="password" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" placeholder="Password confirmation" name="password_confirmation" class="form-control"
+                id="exampleInputPassword1">
+            <label for="exampleInputPassword1" class="form-label">Password confirmation</label>
+        </div>
+        <div class="mb-3">
+            <a href="/login">Log in</a>
+        </div>
+        <button type="submit" class="btn btn-primary">Create account</button>
     </form>
-</body>
-
-</html>
+@endsection
